3103c853f12e93842aea2257904cc449263f1e85
Juliet Haddadin <jhaddadi@c2r2s2.42amman.com>
2025-05-04 13:18:03 +0300

    1

--- minishell.h
+++ minishell.h
@@ -10,21 +10,4 @@
 #include <readline/history.h>  
 #include <stdbool.h> 
 
-typedef struct s_token
-{
-    char *value;
-    char *type;
-    struct s_token *next;
-}   t_token;
-
-void    Getcwd(char *buf, size_t size);
-char	**check_cmd_path(char **cmd, char **envp);
-char	*get_env_path(char **envp);
-char	**get_directories(char **cmd, char *path_env);
-char	*check_cmd_exist(char **cmd, char *path_env);
-char	*free_norm(char **ptr, char *ret);
-void	ft_free(char **matrix);
-char	*free_norm(char **ptr, char *ret);
-
-
 
